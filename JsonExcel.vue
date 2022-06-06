3886133c7d38949afc474c972c5ff1c952e33e3c
pratikpatel <pratikpatelpp802@gmail.com>
2022-06-06 17:53:51 +0530

    Initial commit

--- JsonExcel.vue
+++ JsonExcel.vue
@@ -48,6 +48,10 @@ export default defineComponent({
     header: {
       default: null,
     },
+    // Title(s) for single column data, must be an array where key is the index of columns (ex: ['titleCol0',,TitleCol2])
+    perColumnsHeaders:  {
+      default: null,
+    },
     // Footer(s) for the data, could be a string or an array of strings (multiple footers)
     footer: {
       default: null,
@@ -165,6 +169,15 @@ export default defineComponent({
           '<tr><th colspan="' + colspan + '">${data}</th></tr>'
         );
       }
+      // perColumnsHeaders
+      const perColumnsHeaders = this.perColumnsHeaders;
+      if(Array.isArray(perColumnsHeaders)) {
+          xlsData += "<tr>";
+          for (let pchKey in perColumnsHeaders) {
+              xlsData += "<th>" + perColumnsHeaders[pchKey] + "</th>";
+          }
+          xlsData += "</tr>";
+      }
 
       //Fields
       xlsData += "<tr>";
@@ -217,6 +230,17 @@ export default defineComponent({
       const header = this.header || this.$attrs.title;
       if (header) {
         csvData.push(this.parseExtraData(header, "${data}\r\n"));
+      }
+
+      // perColumnsHeaders
+      const perColumnsHeaders = this.perColumnsHeaders;
+      if(Array.isArray(perColumnsHeaders)) {
+          for (let pchKey in perColumnsHeaders) {
+              csvData.push(perColumnsHeaders[pchKey]);
+              csvData.push(",");
+          }
+          csvData.pop();
+          csvData.push("\r\n");
       }
 
       //Fields
